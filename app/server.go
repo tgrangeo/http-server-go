43ba81b4faa6b1593c82e57b283a595f3f502079
tgrangeo <thomas.grangeon9@gmail.com>
2024-04-14 07:17:37 +0200

    pass stage

--- app/server.go
+++ app/server.go
@@ -29,12 +29,17 @@ func main() {
 	request := string(b)
 	status := strings.Split(request, "\r\n")
 	path := strings.Split(status[0], " ")[1]
+	userAgent := strings.Split(status[2], " ")[1]
 	if path == "/"{
 		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}else if strings.HasPrefix(path, "/echo/"){
 		str := strings.TrimPrefix(path, "/echo/")
 		len := strconv.Itoa(len(str))
 		connection.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + len + "\r\n\r\n" + str + "\r\n"))
+	}else if strings.HasPrefix(path, "/user-agent/"){
+		str := strings.TrimPrefix(path, "/echo/")
+		len := strconv.Itoa(len(str))
+		connection.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + len + "\r\n\r\n" + userAgent + "\r\n"))
 	} else {
 		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
