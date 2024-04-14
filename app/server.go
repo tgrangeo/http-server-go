bacb066508ef0ede8146ceb72300877fa566a907
tgrangeo <thomas.grangeon9@gmail.com>
2024-04-14 07:19:33 +0200

    pass stage

--- app/server.go
+++ app/server.go
@@ -36,7 +36,7 @@ func main() {
 		str := strings.TrimPrefix(path, "/echo/")
 		len := strconv.Itoa(len(str))
 		connection.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + len + "\r\n\r\n" + str + "\r\n"))
-	}else if strings.HasPrefix(path, "/user-agent/"){
+	}else if strings.HasPrefix(path, "/User-Agent/"){
 		len := strconv.Itoa(len(userAgent))
 		connection.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + len + "\r\n\r\n" + userAgent + "\r\n"))
 	} else {
