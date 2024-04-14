aefe86163486508a281a3f5649be57961a62161e
tgrangeo <thomas.grangeon9@gmail.com>
2024-04-14 07:17:54 +0200

    pass stage

--- app/server.go
+++ app/server.go
@@ -37,8 +37,7 @@ func main() {
 		len := strconv.Itoa(len(str))
 		connection.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + len + "\r\n\r\n" + str + "\r\n"))
 	}else if strings.HasPrefix(path, "/user-agent/"){
-		str := strings.TrimPrefix(path, "/echo/")
-		len := strconv.Itoa(len(str))
+		len := strconv.Itoa(len(userAgent))
 		connection.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + len + "\r\n\r\n" + userAgent + "\r\n"))
 	} else {
 		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
